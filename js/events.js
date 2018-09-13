35f001ad671b5f2a021cce3e84916b98443b5f5a
manon jacquin <manon.jacquin@gmail.com>
2018-09-13 17:52:40 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,41 @@
 //define functions here
 
+function getIt(){
+  const clicked = $('p').on('click', function(){
+    alert(`Hey!`)
+    return clicked
+  })
+}
+
+function frameIt(){
+ $('img').on('load', function(){
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt(){
+  const keyPressed = $('input').on('keydown', function(key){
+    if(key.which === 71 ){
+      alert(`g was pressed`)
+    }
+  })
+  return keyPressed
+}
+
+function submitIt(){
+  const submitForm = $('form').on('submit', function(){
+    alert(`Your form is going to be submitted now.`)
+  })
+  return submitForm
+}
+
+
 $(document).ready(function(){
 
 // call functions here
+getIt();
+frameIt();
+pressIt();
+submitIt();
 
 });
