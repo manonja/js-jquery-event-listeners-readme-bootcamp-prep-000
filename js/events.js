941f5b05665d85ae95ee2ac951af725f117fc9a8
manon jacquin <manon.jacquin@gmail.com>
2018-09-13 17:44:02 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,31 @@
 //define functions here
 
+function getIt(){
+  const clicked = $('p').on('click', function(){
+    alert(`Hey!`)
+    return clicked
+  })
+}
+
+function frameIt(){
+ $('img').on('load', function(){
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt(){
+  const keyPressed = $('input').on('load', function(key){
+    if(key.which === )
+  })
+  return keyPressed
+}
+
+
 $(document).ready(function(){
 
 // call functions here
+getIt();
+frameIt();
+pressIt();
 
 });
